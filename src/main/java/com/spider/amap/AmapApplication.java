1d3c8eb8fae58d26161f78afaac5c0f983d85d0b
zhangjun <1580043700@qq.com>
2018-12-03 17:58:28 +0800

    初始化

---
+++ src/main/java/com/spider/amap/AmapApplication.java
@@ -0,0 +1,12 @@
+package com.spider.amap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmapApplication.class, args);
+    }
+}
